31e489b72096f8b448c720bf5da8cd0bcbedca5a
Darwin978 <72097191+Darwin978@users.noreply.github.com>
2024-09-12 20:46:29 -0500

    Delete Estudiante.py

--- Estudiante.py
+++
@@ -1,5 +0,0 @@
-diccionario = {'Darwin':['POO', 'Base de datos'],
-               'Edwin': ['IA', 'Proyectos Tecnologicos']}
-
-def devolverMaterias(estudiante):
-    return diccionario.get(estudiante, "No se encontro el usuario")
